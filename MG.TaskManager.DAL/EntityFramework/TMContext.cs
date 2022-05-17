9feb0c753a57264df5cbd8b25ccd485332219cd7
MaksGovor <maksgovruha@gmail.com>
2022-05-17 18:57:17 +0300

    CB : change initialazer

--- MG.TaskManager.DAL/EntityFramework/TMContext.cs
+++ MG.TaskManager.DAL/EntityFramework/TMContext.cs
@@ -9,9 +9,16 @@ namespace MG.TaskManager.DAL.EntityFramework
         public DbSet<Project> Projects { get; set; }
         public DbSet<Task> Tasks { get; set; }
 
+        static TMContext()
+        {
+            Database.SetInitializer<TMContext>(new DBInitializer());
+        }
+
         public TMContext() : base("DBConnection")
         {
-            Database.Initialize(true);
+            //Database.Initialize(true);
         }
     }
+
+    public class DBInitializer : DropCreateDatabaseIfModelChanges<TMContext> {}
 }
